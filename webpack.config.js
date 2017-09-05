e3f0859a862da299320811dcbf6a9ac7dc8b299a
Pham Trong Phat <phat.phamtrong@lazada.com>
2017-09-05 22:39:49 +0700

    Update configs

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ function prodPlugins(commonPlugins) {
             }
         }),
         new webpack.optimize.AggressiveMergingPlugin(),
-        new webpack.NoErrorsPlugin()
+        new webpack.NoEmitOnErrorsPlugin()
     ])
 }
 
