54546ea83124d7a1c8a0c9d787756ce5e8c17c16
Pham Trong Phat <phat.phamtrong@lazada.com>
2017-10-16 17:02:24 +0700

    update code

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const indexPath = path.resolve(__dirname, 'dist', 'index.html');
 
 app.use(function(req, res, next) {
-    console.log("Request recieved for:", req.url);
     next();
 });
 
 app.use(express.static(path.resolve(__dirname, 'dist')));
 
-// Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+    res.sendFile(indexPath);
 });
 
 app.listen(8080, function() {
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-    BrowserRouter
-} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Container from './components/container/index'
 
 export default () => (
